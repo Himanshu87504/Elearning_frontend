2bdc31d6ef87998dc2e1536029b10d5de0df7b48
Himanshu87504 <165474672+Himanshu87504@users.noreply.github.com>
2025-09-12 10:01:31 +0530

    Update PaymentSuccess.jsx

--- src/pages/paymentsuccess/PaymentSuccess.jsx
+++ src/pages/paymentsuccess/PaymentSuccess.jsx
@@ -4,6 +4,7 @@ import { server } from "../../main";
 import axios from "axios";
 import toast from "react-hot-toast";
 import Loading from "../../components/loading/Loading";
+import { UserData } from "../../context/UserContext";
 
 const PaymentSuccess = ({ user }) => {
   const [loading, setLoading] = useState(true);
